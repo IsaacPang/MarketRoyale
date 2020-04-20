d7ec84972c9cd04683dca2c7d84ef72c25f37a55
Isaac Pang <isaac.pang91@gmail.com>
2020-04-21 01:28:45 +1000

    created 2 versions of json submission generator

---
+++ modules/submit2.py
@@ -0,0 +1,28 @@
+
+import json
+import datetime
+from time import time
+import socket
+from inspect import getsource
+
+DATE = datetime.date.isoformat(datetime.date.today())
+HASH = f"{time():.0f}"[-8:]
+FILE = f"submission_{DATE}_{HASH}.json"
+PATH = f"../submissions/{FILE}"
+
+COMMAND = "ADD" # "ADD", "DEL", "PING"
+SYNDICATE = 8
+TEAM_NAME = "The Rod Polishers"
+
+with open("Player.py", 'r') as f:
+    DATA = f.read()
+
+INPUT = {}
+INPUT["cmd"] = COMMAND
+INPUT["syn"] = SYNDICATE
+INPUT["name"] = TEAM_NAME
+INPUT["data"] = DATA
+
+print(PATH)
+with open(PATH, 'w') as f:
+    json.dump(INPUT, fp=f, indent=4)
