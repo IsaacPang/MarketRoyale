740a7f25f9c98a9d1ea62e79ed692f6b4abed135
Isaac Pang <isaac.pang91@gmail.com>
2020-04-26 21:24:15 +1000

    Edited submissions Author: Isaac Pang
    Multi-line description of commit
    Be as detailed as possible
    
    [Ticket: X]

--- modules/testFile.py
+++ modules/testFile.py
@@ -3,14 +3,14 @@ from Player import Player
 
 
 total_res = []
-for i in range(300):
+for i in range(1):
     p1 = Player()
     p2 = Player()
     p3 = Player()
     p = [p1, p2, p3]
-    g = Game(p, verbose=False)
+    g = Game(p, verbose=True)
     res = g.run_game()
-    #print(res)
+    print(res)
     total_res += res
 
 print(sum(total_res)/len(total_res))
